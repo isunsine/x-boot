0e361ffa4baeccc4a8d6770cc18b5e699c9a04cb
isunsine <isunsine@163.com>
2018-12-29 17:32:39 +0800

    代码合并

--- src/main/java/cn/exrick/xboot/config/security/UserDetailsServiceImpl.java
+++ src/main/java/cn/exrick/xboot/config/security/UserDetailsServiceImpl.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package cn.exrick.xboot.config.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import cn.exrick.xboot.entity.User;
-import cn.exrick.xboot.exception.LoginFailLimitException;
-import cn.exrick.xboot.service.UserService;
-import cn.hutool.core.util.StrUtil;
-
-/**
- * @author Exrickx
- */
-@Component
-public class UserDetailsServiceImpl implements UserDetailsService{
-
-    @Value("${xboot.loginAfterTime}")
-    private Integer loginAfterTime;
-
-    @Autowired
-    private StringRedisTemplate redisTemplate;
-
-    @Autowired
-    private UserService userService;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        String flagKey = "loginFailFlag:"+username;
-        String value = redisTemplate.opsForValue().get(flagKey);
-        if(StrUtil.isNotBlank(value)){
-            //超过限制次数
-            throw new LoginFailLimitException("登录错误次数超过限制，请"+loginAfterTime+"分钟后再试");
-        }
-        User user = userService.findByUsername(username);
-        return new SecurityUserDetails(user);
-    }
-}
-=======
 package cn.exrick.xboot.config.security;
 
 import cn.exrick.xboot.modules.base.entity.User;
@@ -86,4 +41,4 @@ public class UserDetailsServiceImpl implements UserDetailsService{
         return new SecurityUserDetails(user);
     }
 }
->>>>>>> remotes/upstream/master
+
